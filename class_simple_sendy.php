5d35fcb033a19399ee3d1adc1b722edb158c8d58
Code With Mark <mark2kumar@gmail.com>
2019-05-29 14:43:50 -0400

    update the code

--- class_simple_sendy.php
+++ class_simple_sendy.php
@@ -16,6 +16,7 @@ class SimpleSendy
 
 	//--->Get api key - Start 
 	public function __construct($api_key="your_api_key",$base_url = 'https://api.sendgrid.com/api/')
+	//https://api.sendgrid.com/v3/mail/send
 	{ 
 		$this->APIKey = $api_key;	 
 		$this->BaseURL = $base_url;
@@ -23,7 +24,107 @@ class SimpleSendy
 	//--->Get api key -- End
 
 	
-	
+	//--->Transmissions - Start
+
+	function email($call_arr = array(
+		'from_name' =>null,	
+		'from_email' => null,
+		'to_emails' =>array(), 
+		'subject_line' =>null,
+		'email_body' =>null,
+		'future_timestamp' =>null,
+		'category' =>null,
+		'batch_id' =>null,) )
+	{ 
+		$from_name 		= isset($call_arr['from_name']) ? $call_arr['from_name'] : 'GET' ;
+	    $from_email 	= isset($call_arr['from_email']) ? $call_arr['from_email'] : '' ;
+	    $to_emails 		= isset($call_arr['to_emails']) ? $call_arr['to_emails'] : '' ;
+	    $subject_line 	= isset($call_arr['subject_line']) ? $call_arr['subject_line'] : '' ;
+
+	    $email_body 	= isset($call_arr['email_body']) ? $call_arr['email_body'] : '' ;
+
+	    $future_timestamp 	= isset($call_arr['future_timestamp']) ? strtotime($call_arr['future_timestamp']) : date('U') ;
+
+
+	    
+
+		//For transactional email 
+
+		//More info check out: https://sendgrid.com/docs/API_Reference/Web_API/mail.html
+		$batch_id = isset($call_arr['batch_id']) ? $call_arr['batch_id'] : $this->AutoHash();
+		
+		$category 	= isset($call_arr['category']) ? $call_arr['category'] : 'auto_cat_id_'.$batch_id ;
+
+		$timestamp = date('Y-m-d H:i:s',$future_timestamp);
+
+		$json_string = array(
+			'to' => $to_emails,		  
+		  	'category' => $category,
+		  	'send_at'	=> $future_timestamp,
+
+			'unique_args' => array(
+				'batch_id'	=> $batch_id,	
+				'email_dispatch_dttm'=>	$future_timestamp,
+				'timestamp'=>$timestamp,
+			),
+			/*
+			//For constant email footer
+			'filters' => array(
+				'footer' => array(
+					'settings' => array(
+						'enable'	=> $batch_id,
+						'text/html' =>"<p>Thanks,<br />CodeWithMark.com<p>", 
+						'text/plain' =>"<p>Thanks,<br />CodeWithMark.com<p>",
+					),
+				),
+			),
+			*/
+		);
+
+		$params = array(
+			
+			'x-smtpapi' => json_encode($json_string),
+			'fromname' 	=> $from_name,
+			'from'      => $from_email,			
+			'to'        => $from_email, //--> It won't send the email to it... but you still need it for multiple recipients send...
+			//'replyto'	=> $replyto_email,
+			'subject'   => $subject_line,
+			'html'      => $email_body,
+			//'text'      => $email_body,
+			
+		);
+
+		$res = $this->Send( 'mail.send.json', $params ); 
+ 
+		$json = ($res);
+
+	 
+		if( isset($res['data']['errors']) && $res['http_code'] =400 )
+		{
+			$d = array(
+				'status'=>'error-099',				 
+				'msg'=> $res['data']["errors"][0],
+			);
+
+			return  $d;
+		}
+		else if($res['http_code'] =200)
+		{
+			$d = array(
+				'status'=>'success',
+				'id' =>$batch_id,				  				
+			);
+			return  $d;
+		}
+		else 
+		{
+			//send raw response
+			return  $json;
+		}
+	}		
+	//--->Transmissions - End
+
+
 	//--->Send data - Start
 	function Send($url = '', $transmission_data = array() )
 	{	
@@ -37,12 +138,8 @@ class SimpleSendy
 		// Tell curl that this is the body of the POST
 		curl_setopt ($session, CURLOPT_POSTFIELDS, $data_string);
 
-		curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $this->APIKey));
-
-
-		// Tell PHP not to use SSLv3 (instead opting for TLS)
-		//curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-
+		curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $this->APIKey)); 
+	 
 		//Turn off SSL
 		curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);//New line
 		curl_setopt($session, CURLOPT_SSL_VERIFYHOST, false);//New line
@@ -52,153 +149,22 @@ class SimpleSendy
 		// obtain response
 		$result = curl_exec($session);
 
+		$http_code = curl_getinfo($session, CURLINFO_HTTP_CODE);
+		
 		curl_close($session);
+		
+	 
 
-		return $result ;
+		//return $result ;
+		return array('http_code'=> $http_code, 'data' =>  json_decode($result,true));
 		
 	}
 	//--->Send data - End
 
-	
-	//--->Transmissions - Start
-
-	function Mailer($a1 = array(
-		'from_name' =>'' ,
-		'from_email' =>'' ,
-		'to_emails' =>'' ,
-		'subject_line' =>'' ,
-		'email_body' =>array(),
-		'get_category' =>null ,
-		'get_future_timestamp' =>null,
-		'sub' => array() 
-	))
-	{
-		/*
-			$sendy = new SimpleSendy($sendgrid_api_key);
-			$d = $sendy->mailer(array(
-				'from_name' =>'Code With Mark ' ,
-				'from_email' =>'codewithmark@gmail.com' ,
-				'to_emails' =>array('test1@gmail.com','test2@gmail.com'),
-				'subject_line' =>'Test subject line...' ,
-				'email_body' => 'Testing 1232'
-			));
-		*/
-
-		//variables
-		$from_name 				= $a1['from_name'];
-		$from_email 			= $a1['from_email'];
-		$to_emails 				= $a1['to_emails'];		
-		$subject_line 			= $a1['subject_line'];
-		$email_body 			= $a1['email_body']; 
-		$get_future_timestamp 	= $a1['get_future_timestamp'];
-		$get_category 			= $a1['get_category'];
-		$substitution_tags 		= $a1['sub_tags'];
-
-
-		//For transactional email 
-
-		//More info check out: https://sendgrid.com/docs/API_Reference/Web_API/mail.html
-
-		$future_timestamp = isset($get_future_timestamp)? strtotime($get_future_timestamp ) : date('U'); 
-
-		//You can use this to track user email devlivery/open/click events via webhook
-		$batch_id = $this->AutoHash();
-		
-	  
-		$category = isset($get_category)? $get_category : 'auto_cat_id_'.$batch_id;
-
-		$timestamp = date('Y-m-d H:i:s',$future_timestamp);
-	
-		if(isset($substitution_tags))
-		{
-			$json_string = array(
-				'to' => $to_emails,		  
-			  	'category' => $category,
-			  	'sub' => $substitution_tags,
-			  	'send_at'	=> $future_timestamp,
-				'unique_args' => array(
-					'batch_id'	=> $batch_id,					
-				),				 
-			);
-
-		}
-		else
-		{
-			$json_string = array(
-				'to' => $to_emails,		  
-			  	'category' => $category,
-			  	'send_at'	=> $future_timestamp,
-				'unique_args' => array(
-					'batch_id'	=> $batch_id,					
-				),
-				/*
-				//For constant email footer
-				'filters' => array(
-					'footer' => array(
-						'settings' => array(
-							'enable'	=> $batch_id,
-							'text/html' =>"<p>Thanks,<br />CodeWithMark.com<p>", 
-							'text/plain' =>"<p>Thanks,<br />CodeWithMark.com<p>",
-						),
-					),
-				),
-				*/
-			);
-		}
-
-
-		$params = array(			
-			'x-smtpapi' => json_encode($json_string),
-			'fromname' 	=> $from_name,
-			'from'      => $from_email,
-			
-			//'toname'	=> $to_name,
-			'to'        => $from_email, //--> It won't send the email to it... but you still need it for multiple recipients send...
-			//'replyto'	=> $replyto_email,
-			'subject'   => $subject_line,
-			'html'      => $email_body,
-			'text'      => $email_body,
-			
-		);
-
-		$res = $this->Send( 'mail.send.json', $params ); 
-		$json = json_decode($res);
-
-	 
-		if( isset($json->error) )
-		{
-			$d = array(
-				'status'=>'error',				 
-				'message'=> $json,
-			);
-
-			return  $d;
-		}
-		else if($json->message == "success")
-		{
-			$d = array(
-				'status'=>'success',
-				'id' =>$batch_id,				  
-				'message' =>$json
-			);
-			return  $d;
-		}
-		else 
-		{
-			return  $json;
-		}
-	}		
-	//--->Transmissions - End
-
 
 	//--->Local functions - Start
 	function AutoHash($has_name = 'crc32') 
-	{
-	    /*
-	        Use this to create auto/random code
-
-	        More hash options - http://codewithmark.com/best-option-for-hashing-your-passwords-in-php
-	    */
+	{ 
 	    
 	    $alphanum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	    $special  = '~!@#$%^&*(){}[],./?';
@@ -275,4 +241,5 @@ class SimpleSendy
 	    }
 	}
 	//--->Local functions - End 
-} 
+}
+?>
